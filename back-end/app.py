5dc21c375eabfc57d0b8d2fdd318e21b342fcd2b
tdonati <tdonati@bu.edu>
2019-02-21 23:08:24 -0500

    back end

---
+++ back-end/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, request
+import requests
+import json
+from flask import jsonify 
+
+
+app = Flask(__name__)
+
+@app.route('/getUser')
+def hello_world():
+
+
+	url = "https://api.twitch.tv/kraken/users"
+
+	querystring = {"login": request.args.get('streamer')}
+
+	payload = ""
+	headers = {
+	    'Client-ID': "vkl567djmcan9haxeej2z38zi2081f",
+	    'Accept': "application/vnd.twitchtv.v5+json",
+	    'cache-control': "no-cache",
+	    'Postman-Token': "f54c2dc0-acc9-44f4-8ef6-46bc696aae07"
+	    }
+
+	response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+
+	json_data = json.loads(response.text)
+	users = (json_data['users'])
+	#print(users)
+	
+	for user in users:
+		temp_dic = {}
+		temp_dic['display_name'] = user['display_name']
+		temp_dic['_id'] = user['_id']
+		temp_dic['name'] = user['name']
+		temp_dic['logo'] = user['logo']
+
+		url = "https://api.twitch.tv/helix/streams"
+		print(user['_id'])
+		querystring = {"user_id": str(user['_id'])}
+
+		payload = ""
+		headers = {
+		    'Client-ID': "vkl567djmcan9haxeej2z38zi2081f",
+		    'cache-control': "no-cache",
+		    'Postman-Token': "57df971b-f581-4414-9861-c39e546ca6dc"
+		    }
+
+		responseLive = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+
+		json_user = json.loads(responseLive.text)
+		
+		user_data = (json_user['data'])
+
+		if(user_data != []):
+			temp_dic['isLive'] = 'live'
+		else:
+			temp_dic['isLive'] = 'inactive'
+
+		#streamFacts = (json)
+			
+		return(jsonify(temp_dic))
